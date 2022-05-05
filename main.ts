b7d12ad80de14a3828c55d966afe6eeef06516e7
Diegotovar1987 <105001726+Diegotovar1987@users.noreply.github.com>
2022-05-05 10:28:11 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,68 @@
+basic.showIcon(IconNames.Square)
+basic.showLeds(`
+    . # # # #
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
+basic.showLeds(`
+    . . # # #
+    . . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
+basic.showLeds(`
+    . . . # #
+    . . . . #
+    . . . . #
+    # . . . #
+    # # # # #
+    `)
+basic.showLeds(`
+    . . . . #
+    . . . . #
+    . . . . #
+    . . . . #
+    # # # # #
+    `)
+basic.showLeds(`
+    . . . . #
+    . . . # #
+    . . . . #
+    . # . . #
+    # # # # #
+    `)
+basic.showLeds(`
+    . . . . #
+    . . . # #
+    . . # . #
+    . # . . #
+    # # # # #
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # #
+    . . # # #
+    . # # # #
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . #
+    . . . # #
+    . . # # #
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . #
+    . . . # #
+    `)
+basic.clearScreen()
+basic.forever(function () {
+	
+})
